4f00e12321e64e0faa0a67b9c4a4a8cf0c71852a
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-27 00:04:30 +0400

    Added base project code

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gogen",
+	Short: "A CLI tool to generate code scaffolding for day to day tasks",
+	Long: `A CLI interface that can be used to initialize projects 
+and potentially configured to set templates which can be used to 
+create project scaffolding code and utility tools along with having some pre-configured templates.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to gogen!")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gode-gen.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
