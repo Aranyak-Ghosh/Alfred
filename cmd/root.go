2a7e51a851abc3e2a96f0430a97da40123033ffd
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-27 14:07:52 +0400

    Reformatted description

--- cmd/root.go
+++ cmd/root.go
@@ -19,7 +19,8 @@ var rootCmd = &cobra.Command{
 	Short:   "A CLI tool to generate code scaffolding for day to day tasks",
 	Long: `A CLI interface that can be used to initialize projects 
 and potentially configured to set templates which can be used to 
-create project scaffolding code and utility tools along with having some pre-configured templates.`,
+create project scaffolding code and utility tools along with having 
+some pre-configured templates.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Welcome to Alfred!")
 		err := utils.EnsureDependencyInstall()
