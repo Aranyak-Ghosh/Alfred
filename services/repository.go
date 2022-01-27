e56f156082870103d4fb017ca6b03802f7800e77
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-01-27 20:50:05 +0400

    Added services for handling repository collection

--- services/repository.go
+++ services/repository.go
@@ -1,6 +1,10 @@
 package services
 
-import "alfred/utils"
+import (
+	"alfred/models"
+	"alfred/utils"
+	"fmt"
+)
 
 func InitializeRepoStore() error {
 	configPath, err := utils.GetConfigPath()
@@ -16,6 +20,75 @@ func InitializeRepoStore() error {
 	}
 
 	err = utils.WriteFile(configPath+"/repositories.yaml", []byte(""))
+
+	return err
+}
+
+func GetRepoStore() (models.RepoMap, error) {
+	configPath, err := utils.GetConfigPath()
+
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := utils.ReadFile(configPath + "/repositories.yaml")
+
+	if err != nil {
+		return nil, err
+	}
+
+	repos, err := utils.ParseConfigString(config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return repos, nil
+}
+
+func AddRepoToStore(repos map[string]string, overwrite bool) error {
+	currentRepo, err := GetRepoStore()
+
+	if err != nil {
+		return err
+	}
+
+	if !overwrite {
+		for tag, url := range repos {
+			if val, ok := currentRepo[tag]; ok {
+				return fmt.Errorf("Tag %s already exists in repository store. Current URL: %s, New URL: %s", tag, val, url)
+			}
+		}
+	}
+
+	txt, err := utils.SerializeConfig(repos)
+	if err != nil {
+		return err
+	}
+
+	configPath, err := utils.GetConfigPath()
+	if err != nil {
+		return err
+	}
+
+	err = utils.AppendToFile(configPath+"/repositories.yaml", txt)
+	return err
+}
+
+func AddReposToStoreFromFile(filePath string, overwrite bool) error {
+	txt, err := utils.ReadFile(filePath)
+
+	if err != nil {
+		return err
+	}
+
+	repos, err := utils.ParseConfigString(txt)
+
+	if err != nil {
+		return err
+	}
+
+	err = AddRepoToStore(repos, overwrite)
 
 	return err
 }
